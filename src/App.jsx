86f320595c6b5c81d4533e42b0274294c53e364f
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2024-01-13 11:21:18 -0800

    Switched to bun + vite and added some components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Wrapper from "./components/Wrapper/Wrapper.jsx";
+import CalculatorBody from "./components/CalculatorBody/CalculatorBody.jsx";
+
+function App() {
+  return (
+    <>
+      <Wrapper>
+        <CalculatorBody />
+      </Wrapper>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CalculatorBody/CalculatorBody.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CalculatorBody.css";
+
+const CalculatorBody = ({ children }) => {
+  return <div className="calculatorbody">{children}</div>;
+};
+
+export default CalculatorBody;
